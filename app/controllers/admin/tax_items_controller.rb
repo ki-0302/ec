57c639176947e0def1f3aba9ae8113b9a483141d
Maho-ya <support@maho-ya.com>
2019-01-13 21:37:35 +0900

    Changed: tax_items_controller

--- app/controllers/admin/tax_items_controller.rb
+++ app/controllers/admin/tax_items_controller.rb
@@ -1,13 +1,61 @@
-class Admin::TaxItemsController < ApplicationController
-  def index
-  end
+module Admin
+  class TaxItemsController < ApplicationController
+    def index
+      @q = TaxItem.ransack(params[:q])
+      @q.sorts = 'id desc'
+      @tax_items = @q.result(distinct: true).page(params[:page])
+    end
 
-  def show
-  end
+    def show
+      @tax_item = TaxItem.find(params[:id])
+    end
 
-  def edit
-  end
+    def new
+      @tax_item = TaxItem.new
+    end
 
-  def new
+    def create
+      @tax_item = TaxItem.new(tax_item_params)
+
+      if @tax_item.save
+        redirect_to admin_tax_items_path, notice: TaxItem.model_name.human + "「#{@tax_item.name}」を登録しました。"
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @tax_item = TaxItem.find(params[:id])
+    end
+
+    def update
+      @tax_item = TaxItem.find(params[:id])
+      if @tax_item.update(tax_item_params)
+        redirect_to admin_tax_items_path, notice: TaxItem.model_name.human + "「#{@tax_item.name}」を更新しました。"
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      tax_item = TaxItem.find_by(id: params[:id])
+      if tax_item.nil?
+        redirect_index '削除に失敗しました。対象の' + TaxItem.model_name.human + 'は存在しません。'
+      elsif tax_item.destroy
+        redirect_index TaxItem.model_name.human + "「#{tax_item.name}」を削除しました。"
+      else
+        redirect_index '削除に失敗しました。' + fetch_errors(tax_item)
+      end
+    end
+
+    private
+
+    def redirect_index(message)
+      redirect_to admin_tax_items_url, notice: message
+    end
+
+    def tax_item_params
+      params.require(:tax_item).permit(:name, :tax_class_id)
+    end
   end
 end
