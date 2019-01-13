cc4797f552c14f4ef73eed0ad386a6017f94c1dc
Maho-ya <support@maho-ya.com>
2019-01-14 01:21:00 +0900

    Add Product

---
+++ db/migrate/20190113153549_create_products.rb
@@ -0,0 +1,22 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name, null: false
+      t.references :category, foreign_key: true, default: nil
+      t.string :manufacture_name
+      t.string :code
+      t.references :tax_item, foreign_key: true, default: nil
+      t.integer :sales_price
+      t.integer :regular_price
+      t.integer :number_of_stocks
+      t.boolean :unlimited_stock, null: false, default: true
+      t.datetime :display_start_datetime
+      t.datetime :display_end_datetime
+      t.string :description
+      t.string :search_term
+      t.string :jan_code
+      t.integer :status, null: false, default: 0
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/products.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :product do
+    name { 'テスト商品' }
+    association :category
+    manufacture_name { 'テストメーカー' }
+    code { 'ABC-001' }
+    association :tax_item
+    sales_price { 5980 }
+    regular_price { 6200 }
+    number_of_stocks { 100 }
+    unlimited_stock { false }
+    display_start_datetime { '2019-01-20 12:00:00' }
+    display_end_datetime { '2019-06-20 12:00:00' }
+    description { '商品説明です' }
+    search_term { '冬 ライト' }
+    jan_code { '4512345678901' }
+    status { 0 }
+  end
+end
