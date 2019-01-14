bbaab0d6ca322c9e51866dd0575e1ca763cbaa64
Maho-ya <support@maho-ya.com>
2019-01-14 15:51:11 +0900

    Generate controller and view of products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'products/new'
+    get 'products/edit'
+    get 'products/show'
+    get 'products/index'
+  end
+  namespace :admin do
     resources :admin_users
     resources :categories
     resources :tax_classes
