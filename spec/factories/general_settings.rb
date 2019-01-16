7da436138c79711ceaab2c186d3121c3b2782479
Maho-ya <support@maho-ya.com>
2019-01-16 18:43:53 +0900

    Add spec to GeneralSetting

--- spec/factories/general_settings.rb
+++ spec/factories/general_settings.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :general_setting do
-    
+    site_name { 'テストサイト' }
+    postal_code { '100-0000' }
+    region { '東京都' }
+    address1 { '東京市' }
+    address2 { '玉木町１−１−１' }
+    address3 { '報瀬ビル５F' }
+    phone_number { '03-0000-0000' }
   end
 end
--- spec/models/general_setting_spec.rb
+++ spec/models/general_setting_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe GeneralSetting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '追加・更新・削除' do
+    let(:general_setting_name) { 'テストサイト' }
+    let(:general_setting) { FactoryBot.create(:general_setting, name: general_setting_name) }
+
+    describe '追加' do
+      it '一般設定が追加できること' do
+        expect(general_setting).to be_valid
+        expect(GeneralSetting.find_by(name: general_setting_name)).to be_truthy
+      end
+      it '一般設定が複数追加できること' do
+        expect(general_setting).to be_valid
+
+        general_setting2_name = 'テスト一般設定2'
+        general_setting2 = FactoryBot.create(:general_setting, name: general_setting2_name)
+        expect(general_setting2).to be_valid
+        expect(GeneralSetting.find_by(name: general_setting2_name)).to be_truthy
+      end
+    end
+    describe '更新' do
+      it '一般設定が更新できること' do
+        expect(general_setting).to be_valid
+        update_general_setting = GeneralSetting.find_by(name: general_setting_name)
+        expect(update_general_setting).to be_truthy
+        update_general_setting_name = '更新一般設定'
+        update_general_setting.name = update_general_setting_name
+        update_general_setting.save
+        expect(update_general_setting).to be_valid
+        expect(GeneralSetting.find_by(name: update_general_setting_name)).to be_truthy
+      end
+    end
+    describe '削除' do
+      it '一般設定が削除できること' do
+        expect(general_setting).to be_valid
+        delete_general_setting = GeneralSetting.find_by(name: general_setting_name)
+        expect(delete_general_setting).to be_truthy
+        delete_general_setting.destroy
+        expect(GeneralSetting.all.size).to eq 0
+      end
+    end
+  end
 end
