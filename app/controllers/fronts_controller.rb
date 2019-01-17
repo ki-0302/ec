30ae214e90b11939717d3cd6ebf1ce3bc8c958bb
Maho-ya <support@maho-ya.com>
2019-01-17 21:42:11 +0900

    Add controller and view of Front

--- app/controllers/fronts_controller.rb
+++ app/controllers/fronts_controller.rb
@@ -1,7 +1,16 @@
 class FrontsController < ApplicationController
+  layout 'front'
+
   def show
+    name = URI.decode(params[:name]).force_encoding('UTF-8')
+    @product = Product.find_by(name: name)
+    @product.nil? && redirect_index('対象の' + Product.model_name.human + 'は存在しません。')
   end
 
   def index
+  end
+
+  def redirect_index(message)
+    redirect_to :root, notice: message
   end
 end
